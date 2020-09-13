3ccc5786bcaf97aee4352f3544777b55bd457cec
Luke Pearson <luke.pearson@infinityworks.com>
2020-09-13 13:33:56 +0100

    Added index

---
+++ src/index.ts
@@ -0,0 +1,96 @@
+export { DateObject } from './dateObject';
+
+// Comparators
+export { diffCalendarWeeks } from './comparators/diffCalendarWeeks';
+export { diffDays } from './comparators/diffDays';
+export { diffMonths } from './comparators/diffMonths';
+export { diffQuarters } from './comparators/diffQuarters';
+export { diffWeekDays } from './comparators/diffWeekDays';
+export { diffWeeks } from './comparators/diffWeeks';
+export { diffYears } from './comparators/diffYears';
+export { isAfter } from './comparators/isAfter';
+export { isBefore } from './comparators/isBefore';
+export { isEqual } from './comparators/isEqual';
+export { max } from './comparators/max';
+export { min } from './comparators/min';
+
+// Durations
+export { formatDuration } from './durations/formatDuration';
+export { getDuration } from './durations/getDuration';
+
+// Formatters
+export { format } from './formatters/format';
+export { formatDistance } from './formatters/formatDistance';
+export { formatFromNow } from './formatters/formatFromNow';
+
+// Generators
+export { generateMonth } from './generators/generateMonth';
+export { generateRange } from './generators/generateRange';
+export { generateWeek } from './generators/generateWeek';
+export { generateWeekYear } from './generators/generateWeekYear';
+export { generateYear } from './generators/generateYear';
+export { localToday } from './generators/localToday';
+export { utcToday } from './generators/utcToday';
+
+// Modifiers
+export { add } from './modifiers/add';
+export { addDays } from './modifiers/addDays';
+export { addMonths } from './modifiers/addMonths';
+export { addQuarters } from './modifiers/addQuarters';
+export { addWeekDays } from './modifiers/addWeekDays';
+export { addWeeks } from './modifiers/addWeeks';
+export { addYears } from './modifiers/addYears';
+export { reconcile } from './modifiers/reconcile';
+export { set } from './modifiers/set';
+export { setDay } from './modifiers/setDay';
+export { setDayOfWeek } from './modifiers/setDayOfWeek';
+export { setDayOfYear } from './modifiers/setDayOfYear';
+export { setMonth } from './modifiers/setMonth';
+export { setQuarter } from './modifiers/setQuarter';
+export { setWeek } from './modifiers/setWeek';
+export { sub } from './modifiers/sub';
+export { subDays } from './modifiers/subDays';
+export { subMonths } from './modifiers/subMonths';
+export { subQuarters } from './modifiers/subQuarters';
+export { subWeekDays } from './modifiers/subWeekDays';
+export { subWeeks } from './modifiers/subWeeks';
+export { subYears } from './modifiers/subYears';
+
+// Parsers
+export { parseDate } from './parsers/parseDate';
+
+// Queries
+export { getCalendarWeek } from './queries/getCalendarWeek';
+export { getCalendarWeeksInMonth } from './queries/getCalendarWeeksInMonth';
+export { getDayOfWeek } from './queries/getDayOfWeek';
+export { getDayOfYear } from './queries/getDayOfYear';
+export { getDaysInMonth } from './queries/getDaysInMonth';
+export { getDaysInYear } from './queries/getDaysInYear';
+export { getEndOfMonth } from './queries/getEndOfMonth';
+export { getEndOfQuarter } from './queries/getEndOfQuarter';
+export { getEndOfWeek } from './queries/getEndOfWeek';
+export { getEndOfWeekYear } from './queries/getEndOfWeekYear';
+export { getEndOfYear } from './queries/getEndOfYear';
+export { getQuarter } from './queries/getQuarter';
+export { getStartOfDecade } from './queries/getStartOfDecade';
+export { getStartOfMonth } from './queries/getStartOfMonth';
+export { getStartOfQuarter } from './queries/getStartOfQuarter';
+export { getStartOfWeek } from './queries/getStartOfWeek';
+export { getStartOfWeekYear } from './queries/getStartOfWeekYear';
+export { getStartOfYear } from './queries/getStartOfYear';
+export { getWeekOfMonth } from './queries/getWeekOfMonth';
+export { isFriday } from './queries/isFriday';
+export { isLastDayOfMonth } from './queries/isLastDayOfMonth';
+export { isLeapYear } from './queries/isLeapYear';
+export { isMonday } from './queries/isMonday';
+export { isSameMonth } from './queries/isSameMonth';
+export { isSameQuarter } from './queries/isSameQuarter';
+export { isSameWeek } from './queries/isSameWeek';
+export { isSameYear } from './queries/isSameYear';
+export { isSaturday } from './queries/isSaturday';
+export { isSunday } from './queries/isSunday';
+export { isThursday } from './queries/isThursday';
+export { isTuesday } from './queries/isTuesday';
+export { isValid } from './queries/isValid';
+export { isWednesday } from './queries/isWednesday';
+export { isWeekend } from './queries/isWeekend';
